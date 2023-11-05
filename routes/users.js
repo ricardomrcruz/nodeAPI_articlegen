11803c81c0fc304206def585ae932eaa97d5e42a
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2023-11-05 02:27:48 +0100

    postman post interaction

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,32 @@ import express  from "express";
 
 const router = express.Router(); // to initiate the router
 
+const users = [
+    {
+        name: "John",
+        lastName: "Doee",
+        age: 35
+    },
+    {
+        name: "James",
+        lastName: "Arthur",
+        age: 29
+    }
+];
+
 //all routes here are starting with /users
 router.get('/', (req ,res) => {
-    res.send('Hello beatch');
+    console.log(users);
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    console.log('POST ROUTE REACHED');
+
+    console.log(req.body);
+    // users.push();
+
+    res.send('POST ROUTE REACHED');
 });
 
 export default router;
